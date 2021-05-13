eaa262881b070a8c519d75efafb423aab699f893
Monika Sharma <monika2004sharma@gmail.com>
2021-05-14 04:36:36 +0530

    update

--- src/Router/endpoint.js
+++ src/Router/endpoint.js
@@ -1,6 +1,6 @@
-const FRONTEND_ENDPOINT = 'http://localhost:3000';
-const BACKEND_ENDPOINT = 'http://localhost:5000';
-// const FRONTEND_ENDPOINT = 'http://localhost:3001';
-// const BACKEND_ENDPOINT = 'http://localhost:3000';
+const FRONTEND_ENDPOINT = 'https://eazr-login-registration-form.netlify.app/';
+const BACKEND_ENDPOINT = 'https://eazr-login-registration-form.herokuapp.com/';
+// const FRONTEND_ENDPOINT = 'http://localhost:3000';
+// const BACKEND_ENDPOINT = 'http://localhost:5000';
 
 module.exports = { FRONTEND_ENDPOINT, BACKEND_ENDPOINT };
